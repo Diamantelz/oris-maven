f670fbe43adc7cce13d52fccfca9a4fa75ed84ab
Dmitry Miheenko <dmitriy.mikheenko@mail.ru>
2023-10-06 00:02:18 +0300

    minor changes

--- src/main/java/ru/itis/DB/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.DB;
-
-import java.util.List;
-
-public interface UserRepository<T> {
-    void save(String nickname, String email, String password);
-    boolean checkUser(String email, String password);
-    List<T> findAll();
-}
