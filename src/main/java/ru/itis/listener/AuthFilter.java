09fafe0588400d8e616e533acc5c4ed74232f0d6
Dmitry Miheenko <dmitriy.mikheenko@mail.ru>
2023-10-06 00:04:06 +0300

    doFilter method was uncommitted

--- src/main/java/ru/itis/listener/AuthFilter.java
+++ src/main/java/ru/itis/listener/AuthFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-//@WebFilter("/*")
+@WebFilter("/*")
 public class AuthFilter implements Filter {
 
     @Override
@@ -17,28 +17,28 @@ public class AuthFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-//        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-//        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-//        HttpSession session = httpServletRequest.getSession();
-//
-//        Boolean isAuthenticated = false;
-//        boolean sessionExists = session != null;
-//        boolean isLoginPage = ((HttpServletRequest) servletRequest).getRequestURI().equals("/login");
-//
-//        if (sessionExists) {
-//            isAuthenticated = (Boolean) session.getAttribute("authenticated");
-//            if (isAuthenticated == null) {
-//                isAuthenticated = false;
-//            }
-//        }
-//
-//        if (isAuthenticated && !isLoginPage || !isAuthenticated && isLoginPage) {
-//            filterChain.doFilter(servletRequest, servletResponse);
-//        } else if (isAuthenticated && isLoginPage) {
-//            httpServletResponse.sendRedirect("/profile");
-//        } else {
-//            httpServletResponse.sendRedirect("/login");
-//        }
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        HttpSession session = httpServletRequest.getSession();
+
+        Boolean isAuthenticated = false;
+        boolean sessionExists = session != null;
+        boolean isLoginPage = ((HttpServletRequest) servletRequest).getRequestURI().equals("/login");
+
+        if (sessionExists) {
+            isAuthenticated = (Boolean) session.getAttribute("authenticated");
+            if (isAuthenticated == null) {
+                isAuthenticated = false;
+            }
+        }
+
+        if (isAuthenticated && !isLoginPage || !isAuthenticated && isLoginPage) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else if (isAuthenticated && isLoginPage) {
+            httpServletResponse.sendRedirect("/profile");
+        } else {
+            httpServletResponse.sendRedirect("/login");
+        }
     }
 
     @Override
